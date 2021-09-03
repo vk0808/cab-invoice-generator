c34e4df6c2f93b35c7c21e909ce0fb359e4dd1c2
vk0808 <vk4765712@gmail.com>
2021-09-03 16:24:22 +0530

    test: case 4

--- CanInvoiceGeneratorTest/UnitTest1.cs
+++ CanInvoiceGeneratorTest/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace CanInvoiceGeneratorTest
     {
         InvoiceGenerator invoiceGenerator = null;
 
+        // tc-1
         [Test]
         public void GivenDistanceAndTime_ShouldReturnTotalFare()
         {
@@ -18,6 +19,7 @@ namespace CanInvoiceGeneratorTest
             Assert.AreEqual(expected, fare);
         }
 
+        // tc-2
         [Test]
         public void GivenMultipleRides_WhenUsingInvoiceSummaryClass_ShouldReturnInvoiceSummary()
         {
@@ -28,6 +30,7 @@ namespace CanInvoiceGeneratorTest
             Assert.AreEqual(expectedSummary, summary);
         }
 
+        // tc-3
         [Test]
         public void GivenInvoiceGenerator_WhenUsingInvoiceSummaryClass_ShouldReturnInvoiceSummary()
         {
@@ -35,6 +38,18 @@ namespace CanInvoiceGeneratorTest
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, 15);
+            Assert.AreEqual(expectedSummary, summary);
+        }
+
+        // tc-4
+        [Test]
+        public void GivenUserId_WhenInvoivceService_ShouldReturnInvoice()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            invoiceGenerator.AddRides("1241", rides);
+            InvoiceSummary summary = invoiceGenerator.GetInvoiceSummary("1241");
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, "1241");
             Assert.AreEqual(expectedSummary, summary);
         }
     }
