d832855d21e2d637732c83b95448a580f853a95d
vk0808 <vk4765712@gmail.com>
2021-09-03 16:02:57 +0530

    test: case 3

--- CanInvoiceGeneratorTest/UnitTest1.cs
+++ CanInvoiceGeneratorTest/UnitTest1.cs
@@ -27,5 +27,15 @@ namespace CanInvoiceGeneratorTest
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
             Assert.AreEqual(expectedSummary, summary);
         }
+
+        [Test]
+        public void GivenInvoiceGenerator_WhenUsingInvoiceSummaryClass_ShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, 15);
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
