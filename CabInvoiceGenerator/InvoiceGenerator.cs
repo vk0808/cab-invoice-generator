75bf3601a9600398987e9baf2cc9e20fb6894b61
vk0808 <vk4765712@gmail.com>
2021-09-03 14:43:00 +0530

    feat: calculate ride fare

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        RideType rideType;
+        private RideRepository rideRepository;
+        private readonly double MINIMUM_COST_PER_KM;
+        private readonly int COST_PER_TIME;
+        private readonly double MINIMUM_FARE;
+
+        public InvoiceGenerator(RideType rideType)
+        {
+            this.rideType = rideType;
+            this.rideRepository = new RideRepository();
+            try
+            {
+                if (rideType.Equals(RideType.NORMAL))
+                {
+                    this.MINIMUM_COST_PER_KM = 10;
+                    this.COST_PER_TIME = 1;
+                    this.MINIMUM_FARE = 5;
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE, "Invalid ride type");
+            }
+        }
+
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            try
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            catch (CabInvoiceException)
+            {
+                if (rideType.Equals(null))
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE, "Invalid Ride type");
+                }
+                if (distance <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid distance");
+                }
+                if (time < 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid time");
+                }
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+
+
+        public void AddRides(string userId, Ride[] rides)
+        {
+            try
+            {
+                rideRepository.AddRide(userId, rides);
+            }
+            catch (CabInvoiceException)
+            {
+                if (rides == null)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are Null");
+                }
+            }
+        }
+    }
+}
